c444b5ea3cac33fd5271341b71eac521b6c0f82b
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-18 15:20:10 +0200

    test(weapons): add OAM-015 reuse proof

---
+++ tests/unit/items/weapon_reuse_test.cpp
@@ -0,0 +1,31 @@
+/**
+ * Canary - A free and open-source MMORPG server emulator
+ * Copyright (©) 2019–present OpenTibiaBR <opentibiabr@outlook.com>
+ * Repository: https://github.com/opentibiabr/canary
+ * License: https://github.com/opentibiabr/canary/blob/main/LICENSE
+ */
+
+#include <gtest/gtest.h>
+
+#include "items/items.hpp"
+#include "items/weapons/weapons.hpp"
+
+TEST(WeaponReuseTest, PreservesMaximumDamageHelpers) {
+	EXPECT_EQ(Weapons::getMaxMeleeDamage(100, 50), 275);
+	EXPECT_EQ(Weapons::getMaxWeaponDamage(100, 100, 50, 1.0F, true), 445);
+	EXPECT_EQ(Weapons::getMaxWeaponDamage(100, 100, 50, 1.0F, false), 470);
+	EXPECT_EQ(Weapons::getMaxWeaponDamage(100, 100, 0, 1.0F, true), 0);
+}
+
+TEST(WeaponReuseTest, PublishesWandAverageHitMetadata) {
+	ItemType itemType;
+	EXPECT_EQ(itemType.maxHitChance, -1);
+
+	WeaponWand wand;
+	wand.setMinChange(20);
+	wand.setMaxChange(80);
+	wand.configureWeapon(itemType);
+
+	EXPECT_EQ(itemType.maxHitChance, 50);
+	EXPECT_EQ(itemType.combatType, wand.getElementType());
+}
